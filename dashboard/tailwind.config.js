584ea3336c9a71fee43b64e77040f95129d1b7bd
Atria Caesariano <atriact@gmail.com>
2026-06-21 20:08:19 +0700

    theme konfigurasi warna tema dasar dan update favicon tab browser

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#030712',
+        surface: '#0f172a',
+        primary: '#06b6d4',
+        secondary: '#3b82f6'
+      }
+    },
   },
   plugins: [],
 }
